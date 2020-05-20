46275092c378972a9dc6c3390e4b2ff4ec2bc316
Lesaffrea <alainlesaffre@hotmail.com>
2020-05-20 22:28:04 +0800

    Add string

--- test.py
+++ test.py
@@ -224,14 +224,18 @@ def test(cfg,
         maps[c] = ap[i]
     return (mp, mr, map, mf1, *(loss.cpu() / len(dataloader)).tolist()), maps
 
-
+#
+#    Starting of the main procedure and analysis of the command line
+#
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='*.cfg path')
-    parser.add_argument('--data', type=str, default='data/coco2014.data', help='*.data path')
+    #  Change as well
+    #  parser.add_argument('--data', type=str, default='data/coco2014.data', help='*.data path')
+    parser.add_argument('--data', type=str, default='data/ppe.data', help='*.data path')
     #  This could change we load the first weights build for ppe .. 300 epcohs
     # parser.add_argument('--weights', type=str, default='weights/yolov3-spp-ultralytics.pt', help='weights path')
-    parser.add_argument('--weights', type=str, default='weights/weight_ppe_15_05_300.pt.pt', help='weights path')
+    parser.add_argument('--weights', type=str, default='weights/weight_ppe_15_05_300.pt', help='weights path')
     parser.add_argument('--batch-size', type=int, default=16, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
@@ -260,11 +264,13 @@ if __name__ == '__main__':
 
     elif opt.task == 'benchmark':  # mAPs at 320-608 at conf 0.5 and 0.7
         y = []
+        padding =[0]
+        np.savetxt('benchmark.txt',padding ,header="Image size 320, 416,512,608 and iou .5 and .7 \n")
         for i in [320, 416, 512, 608]:  # img-size
-            for j in [0.5, 0.7]:  # iou-thres
+            for j in [0.5, 0.7]:        # iou-thres
                 t = time.time()
                 r = test(opt.cfg, opt.data, opt.weights, opt.batch_size, i, opt.conf_thres, j, opt.save_json)[0]
-                y.append(r + (time.time() - t,))
+                y.append(r + (time.time() - t,)+ i)
         np.savetxt('benchmark.txt', y, fmt='%10.4g')  # y = np.loadtxt('study.txt')
 
     elif opt.task == 'study':  # Parameter study
